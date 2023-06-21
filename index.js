be6ea51151d6b3d40520c898c0ba8ba763268c83
FacuConde <128649179+FacuConde@users.noreply.github.com>
2023-06-21 15:03:02 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,46 +1,10 @@
-document.getElementById("signup-form").addEventListener("submit", function(event) {
-    event.preventDefault();
-    var email = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
-    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailRegex.test(email)) {
-      Swal.fire({
-        title: "Error",
-        text: "El correo electrónico no es válido",
-        icon: "error"
-      });
-      return;
-    }
-    var passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/;
-    if (!passwordRegex.test(password)) {
-      Swal.fire({
-        title: "Error",
-        text: "La contraseña debe tener al menos 8 caracteres, una letra mayúscula, una letra minúscula y un número",
-        icon: "error"
-      });
-      return;
-    }
-    Swal.fire({
-      title: "Registro exitoso",
-      text: "La cuenta ha sido registrada",
-      icon: "success"
-    });
-    document.getElementById("signup-form").reset();
-  });
+var footer = document.querySelector('footer');
+footer.style.display = 'none';
 
-  var prevScrollPos = window.pageYOffset;
-
-  window.addEventListener('scroll', function() {
-    var currentScrollPos = window.pageYOffset;
-    var footer = document.querySelector('footer');
-    
-    if (currentScrollPos > prevScrollPos) {
-      footer.style.display = 'block';
-    } else {
-      footer.style.display = 'none';
-    }
-    
-    prevScrollPos = currentScrollPos;
-  });
-  
-  
+window.addEventListener('scroll', function() {
+  if (window.scrollY > 0) {
+    footer.style.display = 'block';
+  } else {
+    footer.style.display = 'none';
+  }
+});
